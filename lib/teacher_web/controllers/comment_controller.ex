7cf93eae03a5292640a2f75220dbed9214fd3add
ogabriel <gabrieloliver8991@gmail.com>
2020-07-24 22:23:46 -0300

    redirect index to /posts

---
+++ lib/teacher_web/controllers/comment_controller.ex
@@ -0,0 +1,23 @@
+defmodule TeacherWeb.CommentController do
+  use TeacherWeb, :controller
+
+  alias Teacher.Posts
+  alias Teacher.Posts.Post
+
+  def create(conn, %{"post" => post_params}) do
+    case Posts.create_post(post_params) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post created successfully.")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Posts.get_post!(id)
+    render(conn, "show.html", post: post)
+  end
+end
