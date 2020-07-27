ac51ff890a8005e031e80de7936f6300b9037d55
ogabriel <gabrieloliver8991@gmail.com>
2020-07-27 09:29:16 -0300

    add load from database

--- lib/chat_web/channels/water_cooler_channel.ex
+++ lib/chat_web/channels/water_cooler_channel.ex
@@ -1,6 +1,8 @@
 defmodule ChatWeb.WaterCoolerChannel do
   use ChatWeb, :channel
 
+  alias Chat.Chats
+
   @impl true
   def join("water_cooler:lobby", payload, socket) do
     {:ok, socket}
@@ -10,6 +12,8 @@ defmodule ChatWeb.WaterCoolerChannel do
   # broadcast to everyone in the current topic (water_cooler:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Chats.create_message(payload)
+
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
