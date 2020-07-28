a6cfeffe0d9f1cf516d0314b931489cbf09930d6
ogabriel <gabrieloliver8991@gmail.com>
2020-07-27 21:48:24 -0300

    send messages to template

--- lib/chat_web/channels/chat_channel.ex
+++ lib/chat_web/channels/chat_channel.ex
@@ -1,6 +1,8 @@
 defmodule ChatWeb.ChatChannel do
   use ChatWeb, :channel
 
+  alias Chat.Chats
+
   @impl true
   def join("chat:" <> _room, _payload, socket) do
     {:ok, socket}
@@ -10,6 +12,15 @@ defmodule ChatWeb.ChatChannel do
   # broadcast to everyone in the current topic (chat:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    "chat:" <> room = socket.topic
+
+    require IEx
+    IEx.pry()
+
+    payload
+    |> Map.merge(%{"room" => room})
+    |> Chats.create_message()
+
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
