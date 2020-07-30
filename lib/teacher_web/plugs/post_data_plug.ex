cad79df19ba8ac33043f9719671593d1a573ac34
ogabriel <gabrieloliver8991@gmail.com>
2020-07-29 22:03:16 -0300

    convert to module plug

--- lib/teacher_web/plugs/post_data_plug.ex
+++ lib/teacher_web/plugs/post_data_plug.ex
@@ -1,9 +1,13 @@
-defmodule TeacherWeb.PostsDataPlug do
+defmodule TeacherWeb.PostDataPlug do
   import Plug.Conn
 
   alias Teacher.Posts
 
-  def post_count(conn, _opts) do
+  def init(opts) do
+    opts
+  end
+
+  def call(conn, _opts) do
     assign(conn, :post_count, Posts.count_posts())
   end
 end
