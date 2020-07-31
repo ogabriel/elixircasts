16562b33faa54029a38f3185ca1c69dd71db4a18
ogabriel <gabrieloliver8991@gmail.com>
2020-07-30 22:54:59 -0300

    add SecureRandom

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule Teacher.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 5.3"},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:secure_random, "~> 0.5.1"}
     ]
   end
 
