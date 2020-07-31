838534dd69a5f677ae34fde6c4027a9746c73626
ogabriel <gabrieloliver8991@gmail.com>
2020-07-30 21:49:29 -0300

    add function plug

--- lib/teacher_web/controllers/post_controller.ex
+++ lib/teacher_web/controllers/post_controller.ex
@@ -1,8 +1,10 @@
 defmodule TeacherWeb.PostController do
   use TeacherWeb, :controller
 
-  alias Teacher.Posts
+  alias Teacher.{Posts, Accouts}
   alias Teacher.Posts.{Post, Comment}
+
+  plug :check_auth when action in [:new, :create, :edit, :update, :delete]
 
   def index(conn, _params) do
     posts = Posts.list_posts()
@@ -59,5 +61,19 @@ defmodule TeacherWeb.PostController do
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: Routes.post_path(conn, :index))
+  end
+
+  defp check_auth(conn, _opts) do
+    if user_id = get_session(conn, :current_user_id) do
+      current_user = Accouts.get_user!(user_id)
+
+      conn
+      |> assign(:current_user, current_user)
+    else
+      conn
+      |> put_flash(:error, "You need to be signed in to access this page")
+      |> redirect(to: Routes.post_path(conn, :index))
+      |> halt
+    end
   end
 end
