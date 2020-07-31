bafd28a5e9426c0a6ce5bd1fdc9c791e1ea5be12
ogabriel <gabrieloliver8991@gmail.com>
2020-07-30 21:37:44 -0300

    add methods to get user by username

--- lib/teacher/accouts.ex
+++ lib/teacher/accouts.ex
@@ -37,6 +37,9 @@ defmodule Teacher.Accouts do
   """
   def get_user!(id), do: Repo.get!(User, id)
 
+  def get_user_by_username(nil), do: nil
+  def get_user_by_username(username), do: Repo.get_by(User, username: username)
+
   @doc """
   Creates a user.
 
