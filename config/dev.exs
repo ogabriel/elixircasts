a8a04b92de847ae17de7407b6438328d94083cf9
ogabriel <gabrieloliver8991@gmail.com>
2020-08-02 12:17:10 -0300

    update password

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :teacher, Teacher.Repo,
   username: "postgres",
-  password: "root",
+  password: "postgres",
   database: "teacher_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
